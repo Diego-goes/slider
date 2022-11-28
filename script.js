1b6bf9bed55696f0f4584170e22f1947129230e1
Diego R Góes <diego@vsktechnology.com.br>
2022-11-28 16:15:05 -0300

    Reestruturação

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// let div_slider = document.querySelector('#div_slider');
-// let qtdSlides = 5
-// function criarImg(){
-//     let img = document.createElement('img')
-//     img.src = '/calculadora.JPG'
-//     return img
-// }
-
-// for(let i=0; i<qtdSlides; i++) {
-//     div_slider.appendChild(criarImg())
-// }
-// CLicar em cima
+// Clicar
 // Arrastar
 // Soltar
 
@@ -21,52 +10,41 @@
 // encerra eventos e retorna ultima posição
 
 let slides = document.querySelectorAll('.slides')
-// slides[0].style.marginLeft = '-100px'
+let div_slider = document.querySelector('#div_slider')
 
 let posicaoCaixa = 0
-let getPosicaoCaixa = () => { return slides[0].getBoundingClientRect().x }
-let posicaoMouse = 0
-let ultimaPosicao = [0, 0]
-let div_slider = document.querySelector('#div_slider')
+// let getPosicaoCaixa = () => { return slides[0].getBoundingClientRect().x }
+let ultimasPosicoes = [0]
 
 div_slider.addEventListener('mousedown', (e) => {
     div_slider.addEventListener('mousemove', arrastar)
-    console.log(`💠💠💠💠💠💠💠💠💠💠💠💠💠💠💠💠`)
-    console.log(`Inicio: ${getPosicaoCaixa()}`)
 })
 div_slider.addEventListener('mouseup', (e) => {
-    console.log(`Final: ${getPosicaoCaixa()}`)
     div_slider.removeEventListener('mousemove', arrastar)
-    distanciaPx = []
-    ultimaPosicao = [0,0]
+    zerarContador()
 })
-function arrastar(e) {
-    console.log(`posicaoCaixa: ${getPosicaoCaixa()}`)
-    posicaoMouse = e.clientX
-    ultimaPosicao.push(posicaoMouse)
-    console.log(`ultimaPosicao[0]: ${ultimaPosicao[0]}`)
-    console.log(`ultimaPosicao[1]: ${ultimaPosicao[1]}`)
-    if(ultimaPosicao[0] != 0){
-        let distanciaPx = Math.sqrt((ultimaPosicao[0] - ultimaPosicao[1]) ** 2)
-        console.log(`distanciaPx: ${distanciaPx}`)
-        ultimaPosicao[0] < ultimaPosicao[1] ?
-            posicaoCaixa += distanciaPx :
-            posicaoCaixa -= distanciaPx
-        slides[0].style.marginLeft = `${posicaoCaixa}px`
-    }
-    console.log(`PosicaoMouse: ${posicaoMouse}`)
-    ultimaPosicao = [ultimaPosicao[1]]
-    console.log(`--------------------------------`)
+function zerarContador() {
+    distanciaPx = 0
+    ultimasPosicoes = [0]
 }
-
-// slides[0].getBoundingClientRect().x - pega a posição do quadrado
-// let distanciaX = 0
-// div_slider.addEventListener('mousedown', (event)=>{
-//     console.log(event.clientX)
-// })
-// div_slider.addEventListener('mouseup', (event)=>{
-//     console.log(event.clientX)
-// })
-// Criar função que cria elementos IMG e os renderiza
-// Criar função que retorna a distancia que o mouse arrastou no eixo X
-// Criar função que altera a posição das imagens com um parametro
+function arrastar(e) {
+    let posicaoMouse = e.clientX
+    ultimasPosicoes.push(posicaoMouse)
+    if (ultimasPosicoes[0] != 0) {
+        let distanciaPx = calcDistanciaPx(ultimasPosicoes)
+        slides[0].style.marginLeft = `${direcaoMovimento(distanciaPx)}px`
+    }
+    atualizarUltimasPosicoes()
+}
+function atualizarUltimasPosicoes(){
+    ultimasPosicoes = [ultimasPosicoes[1]]
+}
+function calcDistanciaPx() {
+    return Math.sqrt((ultimasPosicoes[0] - ultimasPosicoes[1]) ** 2)
+}
+function direcaoMovimento(distanciaPx) {
+    ultimasPosicoes[0] < ultimasPosicoes[1] ?
+        posicaoCaixa += distanciaPx :
+        posicaoCaixa -= distanciaPx
+    return posicaoCaixa
+}
