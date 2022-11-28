035c5a724a5844b8ae5cc2aba1f801bb6b61164b
Diego R Góes <diego@vsktechnology.com.br>
2022-11-28 15:37:08 -0300

    Funcionando

--- script.js
+++ script.js
@@ -23,31 +23,40 @@
 let slides = document.querySelectorAll('.slides')
 // slides[0].style.marginLeft = '-100px'
 
-let posicaoCaixa = slides[0].getBoundingClientRect().x
+let posicaoCaixa = 0
+let getPosicaoCaixa = () => { return slides[0].getBoundingClientRect().x }
 let posicaoMouse = 0
-let ultimaPosicao = [0,0]
+let ultimaPosicao = [0, 0]
 let div_slider = document.querySelector('#div_slider')
 
 div_slider.addEventListener('mousedown', (e) => {
     div_slider.addEventListener('mousemove', arrastar)
-    console.log(`Inicio: ${posicaoCaixa}`)
+    console.log(`💠💠💠💠💠💠💠💠💠💠💠💠💠💠💠💠`)
+    console.log(`Inicio: ${getPosicaoCaixa()}`)
 })
 div_slider.addEventListener('mouseup', (e) => {
-    console.log(`Final: ${posicaoCaixa}`)
+    console.log(`Final: ${getPosicaoCaixa()}`)
     div_slider.removeEventListener('mousemove', arrastar)
+    distanciaPx = []
+    ultimaPosicao = [0,0]
 })
 function arrastar(e) {
-    console.log(`posicaoCaixa: ${posicaoCaixa}`)
-    ultimaPosicao.push(e.clientX)
+    console.log(`posicaoCaixa: ${getPosicaoCaixa()}`)
     posicaoMouse = e.clientX
-    let distanciaPx = Math.sqrt((ultimaPosicao[0] - ultimaPosicao[1])**2)
-    console.log(`distanciaPx: ${distanciaPx}`)
-    ultimaPosicao[0] < e.clientX ?
-        posicaoCaixa += distanciaPx :
-        posicaoCaixa -= distanciaPx
-    slides[0].style.marginLeft = `${posicaoCaixa}px`
-    console.log(`MouseMove: ${posicaoMouse}`)
+    ultimaPosicao.push(posicaoMouse)
+    console.log(`ultimaPosicao[0]: ${ultimaPosicao[0]}`)
+    console.log(`ultimaPosicao[1]: ${ultimaPosicao[1]}`)
+    if(ultimaPosicao[0] != 0){
+        let distanciaPx = Math.sqrt((ultimaPosicao[0] - ultimaPosicao[1]) ** 2)
+        console.log(`distanciaPx: ${distanciaPx}`)
+        ultimaPosicao[0] < ultimaPosicao[1] ?
+            posicaoCaixa += distanciaPx :
+            posicaoCaixa -= distanciaPx
+        slides[0].style.marginLeft = `${posicaoCaixa}px`
+    }
+    console.log(`PosicaoMouse: ${posicaoMouse}`)
     ultimaPosicao = [ultimaPosicao[1]]
+    console.log(`--------------------------------`)
 }
 
 // slides[0].getBoundingClientRect().x - pega a posição do quadrado
